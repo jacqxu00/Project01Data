00f24147e7a43f606d68ae26cb54108b44649163
Jacqueline Xu <jackiestuy@gmail.com>
2018-04-03 16:38:46 -0400

    split div not working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def root():
     return render_template("index.html")
 
