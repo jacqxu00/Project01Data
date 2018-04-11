15dc84076e828b4a54f8165d6231200fdc802f74
andrewWong21 <awong21@stuy.edu>
2018-04-11 14:50:49 -0400

    table

--- utils/db.py
+++ utils/db.py
@@ -2,13 +2,20 @@ import sqlite3
 import os
 from flask import session
 
-f = "nba.db"
+f = "sports.db"
 db = sqlite3.connect(f)
 c = db.cursor()
 
 # if a item has 0 the user is not using the item. If it is 1 they user is using
 #c.execute('CREATE TABLE IF NOT EXISTS items (user TEXT, item TEXT, playing INTEGER);')
+
+
+#c.execute('CREATE TABLE IF NOT EXISTS nba(team TEXT, player TEXT, position TEXT, height INTEGER, weight INTEGER, bmi FLOAT);')
 db.close()
+
+# converts height from a string to a number
+def convertHeight(height):
+    return height
 
 #changes cash amount
 def getBMI(player):
